8bed7ddaaf04906838e6afdef571fcaaebe10aa1
Alexandra <SHUKLINA_ALEX@MAIL.RU>
2022-11-22 22:59:03 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write ("Start");
+int value = 862;
+
+int a = value / 100;
+int b = (value / 10) % 10;
+int c = value % 10;
+
+int result = b;
+Console.WriteLine (result); 
+Console.WriteLine ("End");
+
